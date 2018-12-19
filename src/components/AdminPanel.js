1c21f4c0ee61d4f888244ae48c7078c8908384ff
pmichalis <piotrmichalis@gmail.com>
2018-12-19 22:18:45 +0100

    order form?

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -3,8 +3,26 @@ import React from 'react';
 class AdminPanel extends React.Component {
     render() {
         return (
-            <div className="adminpanel col-md-4">
-                Panel Administratora
+            <div className="adminpanel col-md-4" >
+                <form>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book name" id="name" name="name" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book author" id="autor" name="autor" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <textarea placeholder="Book description" id="description" name="description" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="checkbox"  id="onStock" name="onStock" className="form-check-input"/>
+                        <label for="bookOnStock" className="form-check-label">On stock</label>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book image" id="image" name="image" className="form-control"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add</button>
+                </form>
             </div>);
     }
 }
