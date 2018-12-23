e5a7ccc9d76e2d4101808c4d9fc77e5e0e467f49
pmichalis <piotrmichalis@gmail.com>
2018-12-23 11:58:33 +0100

    add, remove order

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,10 +2,12 @@ import React from 'react';
 import BookView from './BookView';
 
 class Inventory extends React.Component {
+    
+    
     render() {
 
         const bookListing = this.props.books.map( book => {
-            return <BookView book={book}/>
+            return <BookView book={book} addToOrder={this.props.addToOrder}/>
         });
 
         
