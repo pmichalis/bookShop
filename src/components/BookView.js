063fb89d1c6ac44a69980d0c12b9f19b8e456970
pmichalis <piotrmichalis@gmail.com>
2018-12-23 15:54:30 +0100

    repaired invitory

--- src/components/BookView.js
+++ src/components/BookView.js
@@ -9,6 +9,7 @@ export default class BookView extends React.Component {
             <div>
                 <b>{this.props.book.name}</b><br/>
                 <i>{this.props.book.author}</i><br/>
+                <p>{this.props.book.description}</p>
                 <img src={this.props.book.image} width="75" height="100" alt={this.props.book.name}/> 
                 <button className="btn btn-danger" onClick={ (event) => this.props.addToOrder(this.props.book)}>Add to order</button>
             </div>
