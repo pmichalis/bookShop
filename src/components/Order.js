f7ccf1a11c4c92e723ad8c17f1b688c0058595c7
pmichalis <piotrmichalis@gmail.com>
2019-01-08 23:26:36 +0100

    total price

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,7 +2,17 @@ import React from 'react';
 import OrderView from './OrderView';
 
 class Order extends React.Component {
-   
+    constructor() {
+        super();
+        this.state = {
+            totalPrice : 0
+        };
+      }
+      handleClick(){
+        this.setState({
+            totalPrice : +1//doesnt work
+        })
+    }
    
     render() {
         const orderedBooks = this.props.order.map( order => {
@@ -13,6 +23,9 @@ class Order extends React.Component {
         <div className="order col-md-6">
             <h2>Your order:</h2>
            {orderedBooks}
+           <h6>Your order price: {this.state.totalPrice} pln</h6>
+           <button type="button" onClick={this.handleClick.bind(this)}> Przelicz </button>
+           
         </div>
         );
     }
