8a357fcda3bbc5b0e04ed1b6d28f35fda50f5645
pmichalis <piotrmichalis@gmail.com>
2019-01-09 23:06:32 +0100

    working on price

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import OrderView from './OrderView';
 
+
 class Order extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            totalPrice : 0
-        };
-      }
-      handleClick(){
-        this.setState({
-            totalPrice : +1//doesnt work
-        })
-    }
+
    
     render() {
         const orderedBooks = this.props.order.map( order => {
-            return <OrderView book={order} removeFromOrder={this.props.removeFromOrder}/>
+            return <OrderView book={order} 
+            removeFromOrder={this.props.removeFromOrder}
+            />
         })
 
         return (
         <div className="order col-md-6">
             <h2>Your order:</h2>
            {orderedBooks}
-           <h6>Your order price: {this.state.totalPrice} pln</h6>
-           <button type="button" onClick={this.handleClick.bind(this)}> Przelicz </button>
+           <h6>Your order price: [onwork] pln</h6>
            
         </div>
         );
--- src/components/OrderView.js
+++ src/components/OrderView.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 export default class OrderView extends React.Component {
-
+   
 
     render() {
         return (
@@ -10,7 +10,7 @@ export default class OrderView extends React.Component {
             <div className="col-xs-8">
                 <span><strong>{this.props.book.name} </strong></span><br/>
                 <i>Author: {this.props.book.author}</i><br/>
-                <strong>Price: {this.props.book.price} PLENY</strong>
+                ><strong>Price: {this.props.book.price} PLENY</strong>
             </div>
             <div className="col-xs-4">
                 <button className="btn btn-primary" onClick={ (event) => this.props.removeFromOrder(this.props.book.name)}>Remove</button>
